1c492952630815f69b617e3a37c51f3bd18a1fba
Joyce <JoyceZhu@users.noreply.github.com>
2016-03-03 22:47:55 +0000

    ????

--- site.js
+++ site.js
@@ -4,3 +4,20 @@ $(document).ready(function() {
     console.log("rejoice - jquery is enabled!");
 });
 
+//https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/master/weather.json
+$.getJSON("https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/master/weather.json", function(data) {
+    var items = [];
+    var sum=0;
+    $.each( data, function( key, val ) {
+    if((key==="TimeEST") || (key==="TemperatureF"))
+    {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+        sum += val;
+    }
+    console.log("Sum: " + sum);
+    console.log("Avg: " + sum/items.length);
+    $(".weather").append(items.join(""));
+    });
+    
+    $("???").appendTo("#temperature");
+});
